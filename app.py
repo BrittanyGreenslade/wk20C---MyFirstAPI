4dedbafe5d837cc3fa12d2a33f522d7de3f24843
Brittany <bgreensl@ualberta.ca>
2021-06-12 15:42:01 -0600

    fixed some error messages

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, Response
 import json
-# import traceback
+import traceback
 import dbconnect
 # entry point of flask server
 app = Flask(__name__)
@@ -11,45 +11,73 @@ app = Flask(__name__)
 def get_animals():
     conn = dbconnect.get_db_connection()
     cursor = dbconnect.get_db_cursor(conn)
-    cursor.execute("SELECT name, id FROM animals")
-    animals = cursor.fetchall()
+    animals = None
+    try:
+        cursor.execute("SELECT name, id FROM animals")
+        animals = cursor.fetchall()
+    except:
+        traceback.print_exc()
+        print("Sorry, something went wrong!")
+
     dbconnect.close_db_cursor(cursor)
     dbconnect.close_db_connection(conn)
-    animals_json = json.dumps(animals, default=str)
-    return Response(animals_json, mimetype='application/json', status=200)
-    # for animal in animals:
-    #     print(f"Name: {animal[0]} \nID: {animal[1]} \n")
+    if animals == None:
+        return Response("Error fetching data", mimetype='text/plain', status=500)
+    else:
+        animals_json = json.dumps(animals, default=str)
+        return Response(animals_json, mimetype='application/json', status=200)
 
 
 @app.post("/animals")
 def add_animal():
-    # animal_name: None
     # this saves the animal name that's input into postman in a variable
-    animal_name = request.json['animalName']
+    animal_name = None
+    try:
+        animal_name = request.json['animalName']
+    except:
+        traceback.print_exc()
+        return Response("Data input error", mimetype='text/plain', status=400)
+    if(animal_name == None):
+        return Response("Data input error", mimetype='text/plain', status=400)
     conn = dbconnect.get_db_connection()
     cursor = dbconnect.get_db_cursor(conn)
-    cursor.execute("INSERT INTO animals(name) VALUES(?)", [animal_name, ])
-    conn.commit()
+    animal_id = -1
+    try:
+        cursor.execute("INSERT INTO animals(name) VALUES(?)", [animal_name, ])
+        conn.commit()
+        animal_id = cursor.lastrowid
+    except:
+        traceback.print_exc()
+        print("Sorry, something went wrong!")
     # do the thing and close conn/cursor immediately
     dbconnect.close_db_cursor(cursor)
     dbconnect.close_db_connection(conn)
+    if(animal_id == -1):
+        return Response("Add new animal failed!", mimetype='text/plain', status=500)
+    else:
+        return Response("Your animal has been added!", mimetype='application/json', status=201)
     # don't need id here because it's auto'd
     # I commented this stuff out because we didn't need input from the user??
     # json.dumps(animal_name, default=str)
     # returns success message in text data with status 201 = created
-    return Response("Your animal has been added!", mimetype='application/json', status=201)
 
 
 @app.patch("/animals")
 def edit_animal():
-    # saves the animal name and id that's input into postman in variables
-    animal_id = int(request.json['animalId'])
-    animal_name = request.json['animalName']
+    try:
+        # saves the animal name and id that's input into postman in variables
+        animal_id = int(request.json['animalId'])
+        animal_name = request.json['animalName']
+    except:
+        traceback.print_exc()
     conn = dbconnect.get_db_connection()
     cursor = dbconnect.get_db_cursor(conn)
-    cursor.execute(
-        "UPDATE animals SET name = ? WHERE id = ?", [animal_name, animal_id])
-    conn.commit()
+    try:
+        cursor.execute(
+            "UPDATE animals SET name = ? WHERE id = ?", [animal_name, animal_id])
+        conn.commit()
+    except:
+        traceback.print_exc()
     dbconnect.close_db_cursor(cursor)
     dbconnect.close_db_connection(conn)
     # I commented this stuff out because we didn't need input from the user??
